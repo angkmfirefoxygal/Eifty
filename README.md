4c21ce4c0d1f12984314ff177c1d8da20fa4cbdd
siiion <siwonjeon234@gmail.com>
2025-05-18 23:33:18 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Eifty
+
 This is the git repository for 2025 Eifty wallet project.
---
+++ android/app/src/main/kotlin/com/example/eifty/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eifty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
