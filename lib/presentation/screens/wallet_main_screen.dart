60e5a47f5df2e874dfdd05c35496e9f72b5f4594
siiion <siwonjeon234@gmail.com>
2025-05-29 17:29:35 +0900

    FIX: 잔액 조회 네트워크 변경

--- lib/presentation/screens/wallet_main_screen.dart
+++ lib/presentation/screens/wallet_main_screen.dart
@@ -25,7 +25,12 @@ class _WalletMainScreenState extends State<WalletMainScreen> {
 
   Future<void> loadBalances() async {
     final address = await SecureStorageService.getSelectedWalletAddress();
-    if (address == null) return;
+    print('🔍 현재 선택된 지갑 주소: $address'); // ✅ 확인 로그
+
+    if (address == null) {
+      print('❌ 지갑 주소가 null입니다.');
+      return;
+    }
 
     polBalance = await TransactionService.getPolBalance(address);
     polPrice = await fetchPrice('matic-network');
